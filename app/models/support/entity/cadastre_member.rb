022636a2cf719d4a153098289c77f6ac05e42ea7
Cleuber <cleubercanamari11@gmail.com>
2018-10-02 11:27:29 -0300

    Correcao model entity cadastre_member

--- app/models/support/entity/cadastre_member.rb
+++ app/models/support/entity/cadastre_member.rb
@@ -2,8 +2,10 @@ require_dependency 'support/application_record'
 
 module Support
   module Entity
-    class Member < ApplicationRecord
+    class CadastreMember < ApplicationRecord
       self.table_name = 'sihab.entity_cadastre_members'
+
+      audited
     end
   end
 end
