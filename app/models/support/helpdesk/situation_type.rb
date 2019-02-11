c5296de77c7c255b46301b5894dc75e55e607e30
Elton Silva <elton.chrls@gmail.com>
2019-02-11 11:32:45 -0200

    Implementacao Helpdesk

--- app/models/support/helpdesk/situation_type.rb
+++
@@ -1,9 +0,0 @@
-require_dependency 'support/application_record'
-
-module Support
-  module Helpdesk
-    class SituationType < ApplicationRecord
-      self.table_name = 'extranet.helpdesk_situation_types'      
-    end
-  end
-end
