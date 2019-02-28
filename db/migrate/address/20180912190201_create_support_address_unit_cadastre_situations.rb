3c7dfef89b6968d91ef88e2bf079a9a4339d60bc
Igor Marques <igormarjes@gmail.com>
2019-02-28 16:28:56 -0300

    Atualiacao de migration engine address

--- db/migrate/address/20180912190201_create_support_address_unit_cadastre_situations.rb
+++ db/migrate/address/20180912190201_create_support_address_unit_cadastre_situations.rb
@@ -1,11 +1,11 @@
 class CreateSupportAddressUnitCadastreSituations < ActiveRecord::Migration[5.2]
   def change
-    create_table 'sihab.support_address_unit_cadastre_situations' do |t|
+    create_table 'sihab.address_unit_cadastre_situations' do |t|
       t.string :name
-      t.boolean :status, default: false
-      
+      t.boolean :status, default: true
+
       t.timestamps
-      
+
       t.boolean  :deleted, default: false
       t.datetime :deleted_at, default: nil
     end
