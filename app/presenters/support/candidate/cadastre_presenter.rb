b1d89311eec50b8b6b56765952b9eccbdd0667b5
Elton Silva <elton.chrls@gmail.com>
2019-03-27 11:45:12 -0300

    Construcao de servicos para criacao de tickets

--- app/presenters/support/candidate/cadastre_presenter.rb
+++ app/presenters/support/candidate/cadastre_presenter.rb
@@ -15,16 +15,16 @@ module Support
         name.to_s.mb_chars.downcase.titleize
       end
 
-      def cpf_formated
-        cpf.present? ? cpf.to_s.format_cpf : 'Sem informação de cpf'
-      end
-
       # Situacao cadastral
-      
       def current_situation
         Support::Candidate::CadastreSituation.where(cadastre_id: self.id).order(created_at: :asc).last
-      rescue StandardError
+      rescue Exception => e
         'Sem informação de situação cadastral'
+      end
+
+
+      def current_situation_id
+        current_situation.situation_type.id
       end
       
       def current_situation_name
@@ -33,20 +33,8 @@ module Support
         'Sem informação de situação cadastral'
       end
       
-      # Situacao processual
-
-      def current_procedural
-        Support::Candidate::CadastreProcedural.where(cadastre_id: self.id).order(created_at: :asc).last
-      rescue StandardError => e
-        'Sem informação de situação de processual'
-      end
-
-      def current_procedural_name
-        current_procedural.procedural_type.name
-      rescue StandardError => e
-        'Sem informação de situação de processual'
-      end
-
+      
+      # Dados básicos
       def age
         born.present? ? ((Date.today - born).to_i / 365.25).to_i : 'Sem informação de idade.'
       end
--- app/services/support/attendance/ticket_service.rb
+++ app/services/support/attendance/ticket_service.rb
@@ -1,22 +1,80 @@
-module Attendance
-  class TicketService
-    
-    attr_accessor :ticket_id
+module Support
+  module Attendance
+    class TicketService
+      
+      attr_accessor :cadastre, :cadastre_mirror, :ticket, :errors
 
-    def initialize(ticket_id)
-      @ticket_id = ticket_id
-    end
+      def initialize(cadastre_id: nil, ticket_id: nil)
+        @cadastre = Support::Candidate::Cadastre.find_by(id: cadastre_id)
+        @ticket   = Support::Attendance::Ticket.find_by(id: ticket_id)
+        @errors   = []
+      end
 
-    def set_cadastre_mirror!
-      ticket   = Support::Attendance::Ticket.find_by(@ticket_id)
-      service  = Candidate::CadastreService.new(ticket.cadastre_id)
+      def categories_allowed
+        categories = Support::Attendance::TicketCategory.status_active.all
 
-      service.create_mirror!
+        categories.each do |all|
+        end
+      end
 
-      ticket.update(cadastre_mirror_id: service.cadastre_mirror.id)
-    rescue StandarError => e
-      p e
-    end
+      def category_allowed?(category_id)
+        category = Support::Attendance::TicketCategory.status_active.find_by(id: category_id)
+        
+        return false if category.nil?
+        
+        if category.filter_situation
+          if !category.filter_situation_id.include?(@cadastre.presenter.current_situation_id)
+            @errors << "CPF não está vínculado a situação exigida para este atendimento."
+            return false
+          end
+        end
 
-  end 
+        if category.filter_convocation
+          if !category.filter_convocation_id.include?(@cadastre.presenter.current_convocation_id)
+            @errors << "CPF não está vínculado a convocação exigida para este atendimento."
+            return false
+          end
+        end
+
+        if category.filter_program
+          if !category.filter_program_id.include?(@cadastre.program_id)          
+            @errors << "CPF não está vínculado ao programa exigido para este atendimento."
+            return false
+          end
+        end
+
+        if category.filter_sub_program
+          if category.filter_sub_program_id.include?(@cadastre.sub_program_id)  
+            @errors << "CPF não está vínculado ao sub-programa exigido para este atendimento."
+            return false
+          end        
+        end
+
+        #if category.filter_sql
+          # TODO: Realizar criacao 
+        #end
+
+        if category.due
+          if !((category.started_at >= Date.today && category.ended_at <= Date.today))
+            @errors << "Categoria fora do período especificado para atendimento."
+            return false
+          end
+        end
+
+      end
+
+      private
+
+      def generate_cadastre_mirror
+        service  = Candidate::CadastreService.new(cadastre_id: @cadastre.id)
+        service.create_mirror!
+
+        @cadastre_mirror = service.cadastre_mirror
+      rescue Exception => e
+        p e
+      end
+
+
+    end 
+  end
 end
