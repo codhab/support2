54d54ffbaf7df77e5728953af2a2c4d998226926
cleuber <cleubercanamari11@gmail.com>
2019-04-24 09:54:08 -0300

    ok

--- app/models/support/candidate/dependent.rb
+++ app/models/support/candidate/dependent.rb
@@ -5,6 +5,8 @@ module Support
     class Dependent < ApplicationRecord # :nodoc:
       self.table_name = 'sihab.candidate_dependents'
 
+       belongs_to :kinship, class_name: 'Support::Common::Kinship', required: false
+
       def presenter
         call_presenter('Support::Candidate::DependentPresenter', self)
       end
